4f8f7d2f71a317d70f9a5db10b3e4e7b8ab47b44
Miarantsoa <miarantsoaainaharison@gmail.com>
2026-02-06 14:11:23 +0300

    frontend: clean app.module

--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -5,26 +5,13 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import {HttpClientModule} from '@angular/common/http';
 import {FullCalendarModule} from '@fullcalendar/angular';
-import {PlanningComponent} from '../admin/planning/planning.component';
-import {ShopRequestComponent} from '../admin/shop-request/shop-request.component';
 import {FormsModule} from '@angular/forms';
-import {ShopRequestListComponent} from '../admin/shop-request/shop-request-list.component';
-import {PlanningAddComponent} from '../admin/planning/planning-add.component';
 import { SidebarComponent } from '@/shared/components/sidebar/sidebar.component';
-import {ZardButtonComponent} from '@/shared/components/button';
-import {ZardSkeletonComponent} from '@/shared/components/skeleton';
 import {ZardDividerComponent} from '@/shared/components/divider';
-import {ZardAvatarComponent} from '@/shared/components/avatar';
-import {ZardIconComponent} from '@/shared/components/icon';
-import {LayoutComponent} from '@/shared/components/layout';
 
 @NgModule({
   declarations: [
     AppComponent,
-    PlanningComponent,
-    PlanningAddComponent,
-    ShopRequestComponent,
-    ShopRequestListComponent
   ],
   imports: [
     BrowserModule,
