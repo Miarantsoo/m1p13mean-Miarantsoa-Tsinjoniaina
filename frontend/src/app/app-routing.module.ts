c23f784eecf9a8f9cb7bd4cdba88250376105e96
Miarantsoa <miarantsoaainaharison@gmail.com>
2026-02-06 11:58:47 +0300

    frontend: create new route system

--- frontend/src/app/app-routing.module.ts
+++ frontend/src/app/app-routing.module.ts
@@ -4,12 +4,18 @@ import {PlanningComponent} from '../admin/planning/planning.component';
 import {ShopRequestComponent} from '../admin/shop-request/shop-request.component';
 import {ShopRequestListComponent} from '../admin/shop-request/shop-request-list.component';
 import {PlanningAddComponent} from '../admin/planning/planning-add.component';
+import {ROUTES} from '@/core/configs/navigation/navigation.constant';
 
 const routes: Routes = [
-  { path: 'planning', component: PlanningComponent },
-  { path: 'planning/add', component: PlanningAddComponent },
-  { path: 'shop-request', component: ShopRequestComponent },
-  { path: 'shop-request/list', component: ShopRequestListComponent },
+  {
+    path: ROUTES.ADMIN,
+    loadChildren: () =>
+      import('./modules/admin/admin.module').then(
+        (m) => m.AdminModule,
+      ),
+    // canActivate: [AuthGuard],
+    // data: { roles: ['PARAMETRAGE'] },
+  }
 ];
 
 @NgModule({
--- frontend/src/app/modules/admin/admin-routing.module.ts
+++ frontend/src/app/modules/admin/admin-routing.module.ts
@@ -1,12 +1,42 @@
 import { NgModule } from '@angular/core';
-import { CommonModule } from '@angular/common';
+import {RouterModule, Routes} from '@angular/router';
+import {LayoutComponent} from '@/shared/components/layout';
+import {PlanningComponent} from '@/modules/admin/planning/pages/planning/planning.component';
+import {PlanningAddComponent} from '@/modules/admin/planning/pages/planning-add/planning-add.component';
+import {ShopRequestComponent} from '@/modules/admin/shop-request/pages/shop-request/shop-request.component';
+import {
+  ShopRequestListComponent
+} from '@/modules/admin/shop-request/pages/shop-request-list/shop-request-list.component';
 
 
+const routes: Routes = [
+  {
+    path: '',
+    component: LayoutComponent,
+    children : [
+      {
+        path: 'planning',
+        component: PlanningComponent
+      },
+      {
+        path: 'planning/add',
+        component: PlanningAddComponent
+      },
+      {
+        path: 'shop-request',
+        component: ShopRequestComponent
+      },
+      {
+        path: 'shop-request/list',
+        component: ShopRequestListComponent
+      },
+    ]
+  }
+];
 
 @NgModule({
   declarations: [],
-  imports: [
-    CommonModule
-  ]
+  imports: [RouterModule.forChild(routes)],
+  exports: [RouterModule]
 })
 export class AdminRoutingModule { }
