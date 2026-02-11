42d83ea19c39e5e1e97114c2f2f45bb8f6e1c617
Miarantsoa <miarantsoaainaharison@gmail.com>
2026-02-11 10:30:26 +0300

    frontend: add route and import to module

--- frontend/src/app/modules/customer/customer.module.ts
+++ frontend/src/app/modules/customer/customer.module.ts
@@ -1,14 +1,39 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { CustomerRoutingModule } from './customer-routing.module';
+import { LoginComponent } from './auth/pages/login/login.component';
+import {RegisterComponent} from '@/modules/customer/auth/pages/register/register.component';
+import {ReactiveFormsModule} from '@angular/forms';
+import {ZardButtonComponent} from '@/shared/components/button';
+import {
+  ZardFormControlComponent,
+  ZardFormFieldComponent,
+  ZardFormLabelComponent,
+  ZardFormMessageComponent
+} from '@/shared/components/form';
+import {ZardInputDirective} from '@/shared/components/input';
+import {ZardCardComponent} from '@/shared/components/card';
+import {ZardAlertComponent} from '@/shared/components/alert';
 
 
 
 @NgModule({
-  declarations: [],
+  declarations: [
+    RegisterComponent,
+    LoginComponent
+  ],
   imports: [
     CommonModule,
-    CustomerRoutingModule
+    CustomerRoutingModule,
+    ReactiveFormsModule,
+    ZardButtonComponent,
+    ZardFormFieldComponent,
+    ZardInputDirective,
+    ZardCardComponent,
+    ZardAlertComponent,
+    ZardFormControlComponent,
+    ZardFormLabelComponent,
+    ZardFormMessageComponent
   ]
 })
 export class CustomerModule { }
