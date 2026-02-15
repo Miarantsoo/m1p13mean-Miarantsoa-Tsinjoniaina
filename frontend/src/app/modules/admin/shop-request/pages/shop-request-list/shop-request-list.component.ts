b39369a98a0d4808abd1ae41ffe5281001f298c3
Miarantsoa <miarantsoaainaharison@gmail.com>
2026-02-15 18:48:42 +0300

    frontend: status integration to shop request list

--- frontend/src/app/modules/admin/shop-request/pages/shop-request-list/shop-request-list.component.ts
+++ frontend/src/app/modules/admin/shop-request/pages/shop-request-list/shop-request-list.component.ts
@@ -1,6 +1,5 @@
-import { Component, OnInit } from '@angular/core';
-import {Router} from '@angular/router';
-import {ShopRequestService} from '@/modules/admin/shop-request/services/shop-request.service';
+import { Component, ViewChild } from '@angular/core';
+import {ShopRequestStatusListComponent} from '@/modules/admin/shop-request/components/shop-request-status-list/shop-request-status-list.component';
 
 @Component({
   selector: 'app-shop-request-list',
@@ -8,34 +7,17 @@ import {ShopRequestService} from '@/modules/admin/shop-request/services/shop-req
   templateUrl: './shop-request-list.component.html',
   styleUrls: ['./shop-request-list.component.scss']
 })
-export class ShopRequestListComponent implements OnInit {
+export class ShopRequestListComponent {
+  @ViewChild('newTab') newTab!: ShopRequestStatusListComponent;
+  @ViewChild('pendingTab') pendingTab!: ShopRequestStatusListComponent;
+  @ViewChild('rejectedTab') rejectedTab!: ShopRequestStatusListComponent;
 
-  shopRequests: any[] = [];
-  loading = true;
+  onTabChange(event: any): void {
+    const tabComponents = [this.newTab, this.pendingTab, this.rejectedTab];
+    const activeComponent = tabComponents[event.index];
 
-  constructor(
-    private shopRequestService: ShopRequestService,
-    private router: Router
-  ) {}
-
-  ngOnInit(): void {
-    this.shopRequestService.getAll().subscribe({
-      next: (data) => {
-        console.log('shop_request:', data);
-        this.shopRequests = data;
-        this.loading = false;
-      },
-      error: (err) => {
-        console.error(err);
-        this.loading = false;
-      }
-    });
-  }
-
-  goToPlanning(shopRequestId: string) {
-    this.router.navigate(
-      ['/planning/add'],
-      { queryParams: { shop_request: shopRequestId } }
-    );
+    if (activeComponent) {
+      activeComponent.loadData();
+    }
   }
 }
