d4ab90a662b38f7da1344cadc54b075ac5b31fc6
Miarantsoa <miarantsoaainaharison@gmail.com>
2026-02-19 20:36:20 +0300

    backend: update planning controller

--- backend/src/admin/planning/planning.controller.js
+++ backend/src/admin/planning/planning.controller.js
@@ -1,5 +1,6 @@
 import { getAllPlannings, addPlanning } from "./planning.service.js";
 import Planning from "./planning.model.js";
+import ShopRequest from "../shopRequest/shopRequest.model.js";
 import {sendPlanningEmail} from "@/utils/mail.service.js";
 
 export const getPlannings = async (req, res) => {
@@ -13,13 +14,19 @@ export const getPlannings = async (req, res) => {
 
 export const createPlanning = async (req, res) => {
     try {
-        const planning = await addPlanning(req.body);
+        const shopRequest = await ShopRequest.findById(req.body.shop_request);
 
-        const populatedPlanning = await Planning.findById(planning._id)
-            .populate("shop_request");
+        const data = {
+            ...req.body,
+            shop_request: shopRequest
+        }
+        const planning = await addPlanning(data);
+        const populatedPlanning = await Planning.findById(planning._id);
+        shopRequest.status = "pending";
+        await shopRequest.save();
 
         await sendPlanningEmail({
-            to: populatedPlanning.shop_request.email,
+            recipient: shopRequest.email,
             date: populatedPlanning.date,
             duration: populatedPlanning.duration
         });
