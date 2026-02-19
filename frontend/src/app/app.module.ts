d230441575bc9f8c365eba811f274f74cb2e4745
Miarantsoa <miarantsoaainaharison@gmail.com>
2026-02-19 20:43:15 +0300

    frontend: update imports on app module

--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -3,11 +3,13 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import {HttpClientModule} from '@angular/common/http';
+import { HttpClientModule, HTTP_INTERCEPTORS } from '@angular/common/http';
 import {FullCalendarModule} from '@fullcalendar/angular';
 import {FormsModule} from '@angular/forms';
 import { SidebarComponent } from '@/shared/components/sidebar/sidebar.component';
 import {ZardDividerComponent} from '@/shared/components/divider';
+import { AuthInterceptor } from '@/core/services/http/auth-interceptor';
+import {ZardToastComponent} from '@/shared/components/toast';
 
 @NgModule({
   declarations: [
@@ -21,8 +23,15 @@ import {ZardDividerComponent} from '@/shared/components/divider';
     FormsModule,
     ZardDividerComponent,
     SidebarComponent,
+    ZardToastComponent,
   ],
-  providers: [],
+  providers: [
+    {
+      provide: HTTP_INTERCEPTORS,
+      useClass: AuthInterceptor,
+      multi: true
+    }
+  ],
   bootstrap: [
     AppComponent
   ]
