caeed927018f27af928303d2f583cca6105d2d21
Miarantsoa <miarantsoaainaharison@gmail.com>
2026-03-01 08:46:39 +0300

    frontend: implement route guard

--- frontend/src/app/core/guards/auth.guard.ts
+++ frontend/src/app/core/guards/auth.guard.ts
@@ -1,5 +1,18 @@
-import { CanActivateFn } from '@angular/router';
+import {CanActivateFn, Router} from '@angular/router';
+import {inject} from '@angular/core';
+import {AuthService} from '@/core/services/http/auth.service';
 
 export const authGuard: CanActivateFn = (route, state) => {
+  const router = inject(Router);
+  const authService = inject(AuthService);
+
+  const isAuthenticated = authService.isAuthenticated();
+
+  if (!isAuthenticated) {
+    return router.createUrlTree(['/auth/login'], {
+      queryParams: { returnUrl: state.url }
+    });
+  }
+
   return true;
 };
